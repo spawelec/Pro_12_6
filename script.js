bde4430c317134f3c1a4e6177e3cbe245fa998b3
Sylwia <sylwia.pawelec@wp.pl>
2017-11-27 22:21:14 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+var url = 'https://restcountries.eu/rest/v1/name/';
+var countriesList = $('#countries');
+
+function searchCountries() {
+    var countryName = $('#country-name').val();
+if(!countryName.length) countryName = 'Poland';
+$.ajax({
+         url: url + countryName,
+         method: 'GET',
+         success: showCountriesList
+     }); 
+}
+$('#search').click(searchCountries);
+
+function showCountriesList(resp) {
+        countriesList.empty();
+        resp.forEach(function(item){
+             $('<li>').text(item.name).appendTo(countriesList);
+        });
+} 
+
+
+                   
+
+
+
+
+     
