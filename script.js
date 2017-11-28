643fff2f53d5c9c2212588cafd1edfae0ad4db00
Sylwia <Sylwia>
2017-11-28 20:44:14 +0100

    modified

--- script.js
+++ script.js
@@ -3,12 +3,14 @@ var countriesList = $('#countries');
 
 function searchCountries() {
     var countryName = $('#country-name').val();
-if(!countryName.length) countryName = 'Poland';
+if(!countryName.length) { 
+    countryName = 'Poland';
+}
 $.ajax({
-         url: url + countryName,
-         method: 'GET',
-         success: showCountriesList
-     }); 
+        url: url + countryName,
+        method: 'GET',
+        success: showCountriesList
+}); 
 }
 $('#search').click(searchCountries);
 
